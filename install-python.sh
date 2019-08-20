47d07694783066e117caa8bef14248be4ff4758e
xup6g4vm0 <jerrylin1121@gapp.nthu.edu.tw>
2019-08-20 11:32:27 +0800

    commit python install script

---
+++ install-python.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir .my-python3
+cd .my-python3
+wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+tar zxvf Python-3.7.4.tgz
+cd Python-3.7.4
+./configure --prefix=$HOME/.my-python3/Python-3.7.4
+make
+make install
+echo >> ~/.bash_profile
+echo 'PATH=$HOME/.my-python3/Python-3.7.4/bin/:$PATH' >> ~/.bash_profile
+. ~/.bash_profile
+pip3 install virtualenv
+cd ~
+~/.my-python3/Python-3.7.4/bin/virtualenv -p ~/.my-python3/Python-3.7.4/bin/python3 ENV
